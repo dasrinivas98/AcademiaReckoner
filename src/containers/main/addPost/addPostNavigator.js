322c75d814fb374f87ec40de31f562a60091a040
dasrinivas98 <56031256+dasrinivas98@users.noreply.github.com>
2021-12-15 16:14:55 +0530

    added:addMarks

--- src/containers/main/addPost/addPostNavigator.js
+++ src/containers/main/addPost/addPostNavigator.js
@@ -3,6 +3,9 @@ import {createStackNavigator} from '@react-navigation/stack';
 import addPostScreen from './addPostScreen';
 import colors from 'res/colors';
 import {View, Text} from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
 
 export default function addPostNavigator() {
   const Stack = createStackNavigator();
@@ -13,6 +16,13 @@ export default function addPostNavigator() {
         name="Add Post"
         component={addPostScreen}
         options={{
+          headerTitle: (
+            <TouchableOpacity
+              style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+              <Text style={{color:'grey',fontWeight:'bold',fontFamily:'sans-serif-thin',letterSpacing:5}}>Add Marks</Text>
+            </TouchableOpacity>
+          ),
+          headerTitleStyle: {alignSelf: 'center'},
           headerStyle: {
             backgroundColor: colors.bottomBackGround,
             shadowColor: colors.seperatorLineColor,
--- src/containers/main/addPost/addPostScreen.js
+++ src/containers/main/addPost/addPostScreen.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {View, Text} from 'react-native';
 import palette from 'res/palette';
+import colors from 'res/colors';
+import DropDownPicker from 'react-native-dropdown-picker';
+import NumericInput from 'react-native-numeric-input-counter';
 
 export default function addPostScreen() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: 1, value: 1},
+    {label: 2, value: 2},
+    {label: 3, value: 3},
+    {label: 4, value: 4},
+    {label: 5, value: 5},
+    {label: 6, value: 6},
+    {label: 7, value: 7},
+    {label: 8, value: 8},
+
+  ]);
+
   return (
-    <View style={palette.container.center}>
-      <Text style={palette.text}>Add Marks</Text>
+    <View style={{flex: 1, backgroundColor: colors.bottomBackGround}}>
+      <View style={{marginBottom: 5}}>
+        <Text style={{color: 'white'}}>Semester</Text>
+          <DropDownPicker
+            placeholder="Select Semester"
+            open={open}
+            value={value}
+            items={items}
+            setOpen={setOpen}
+            setValue={setValue}
+            setItems={setItems}
+            theme="DARK"
+          />
+      </View>    
+      <View style={{marginBottom: 5}}>
+        <Text style={{color: 'white'}}>Total No. of Subjects</Text>
+        <NumericInput
+          onChange={value => console.log(value)} rounded textColor='#59656F' iconStyle={{ color: 'white' }} rightButtonBackgroundColor='#292d3e' leftButtonBackgroundColor='#292d3e' minValue={1}
+          maxValue={10} />
+      </View>
     </View>
   );
 }
