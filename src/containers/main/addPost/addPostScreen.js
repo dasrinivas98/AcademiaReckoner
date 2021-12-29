91f269d0c426476f6c0504c144448c749fa38f48
dasrinivas98 <56031256+dasrinivas98@users.noreply.github.com>
2021-12-29 15:09:57 +0530

    Update addPostScreen.js
    added dynamic marks add

--- src/containers/main/addPost/addPostScreen.js
+++ src/containers/main/addPost/addPostScreen.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import {View, Text, Button,TouchableOpacity} from 'react-native';
+import {View, Text, Button,TouchableOpacity,ScrollView} from 'react-native';
 import palette from 'res/palette';
 import colors from 'res/colors';
 import DropDownPicker from 'react-native-dropdown-picker';
@@ -11,6 +11,9 @@ export default function addPostScreen() {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
   const [isModalVisible, setModalVisible] = useState(false);
+  const [subCount,setSubCount] = useState(0);
+  const [textInput,setTextInput] = useState([]);
+  
 
   const [items, setItems] = useState([
     {label: 1, value: 1},
@@ -24,11 +27,46 @@ export default function addPostScreen() {
 
   ]);
   const toggleModal = () => {
+    let textInput1 = [];
+    for(let i=0;i<subCount;i++){
+    textInput1.push(<View style={{marginBottom: 5}}key={i}>
+      <Text style={{color: 'white'}}>Subject {i+1}</Text>
+        <TextInput
+        mode='flat'
+        style={{backgroundColor: 'black'}}
+        activeOutlineColor='white'
+        activeUnderlineColor = 'white'
+        outlineColor = 'white'
+        //underlineColor = 'white'
+        selectionColor = 'white'
+        theme={{ colors: { text: 'white',placeholder: "white" } }}
+      />
+      <Text style={{color: 'white',marginTop:10}}>Credits Assigned</Text>
+      <NumericInput
+        totalWidth={240} 
+        totalHeight={50}
+          onChange={value => console.log(value)} rounded textColor='#59656F' iconStyle={{ color: '#59656F' }} rightButtonBackgroundColor='#292d3e' leftButtonBackgroundColor='#292d3e' minValue={1}
+          maxValue={10}/>
+      <Text style={{color: 'white',marginTop:10}}>Marks Obtained</Text>
+        <TextInput
+        mode='flat'
+        keyboardType='numeric'
+        style={{backgroundColor: 'black'}}
+        activeOutlineColor='white'
+        activeUnderlineColor = 'white'
+        outlineColor = 'white'
+        //underlineColor = 'white'
+        selectionColor = 'white'
+        theme={{ colors: { text: 'white',placeholder: "white" } }}
+      />
+    </View>);
+    }
+    setTextInput(textInput1);
     setModalVisible(!isModalVisible);
   };
 
   return (
-    <View style={{flex: 1, backgroundColor: colors.bottomBackGround,padding:10}}>
+    <View style={{flex: 1, backgroundColor: colors.bottomBackGround}}>
       <View style={{marginBottom: 10,marginStart: 10}}>
         <Text style={{color: 'white',marginBottom: 5}}>Semester</Text>
           <DropDownPicker
@@ -47,7 +85,7 @@ export default function addPostScreen() {
         <NumericInput
         totalWidth={240} 
         totalHeight={50}
-           onChange={value => console.log(value)} rounded textColor='#59656F' iconStyle={{ color: '#59656F' }} rightButtonBackgroundColor='#292d3e' leftButtonBackgroundColor='#292d3e' minValue={1}
+           onChange={value => setSubCount(value)} rounded textColor='#59656F' iconStyle={{ color: '#59656F' }} rightButtonBackgroundColor='#292d3e' leftButtonBackgroundColor='#292d3e' minValue={1}
           maxValue={10}/>
       </View>
       <View style={{marginBottom: 10,marginStart: 10}}>
@@ -56,12 +94,12 @@ export default function addPostScreen() {
             <Text style={{color: '#fff'}}>Next</Text>
             {/* <Icon style={Styles.loginText} name='chevron-right' type='font-awesome-5' color='#FFFFFF'/> */}
           </TouchableOpacity>
-          <Modal isVisible={isModalVisible} style={{backgroundColor:'#121212'}} coverScreen={true}>
-            <View style={{ flex: 1 }}>
+          <Modal isVisible={isModalVisible} style={{backgroundColor:'#121212',padding:10}} coverScreen={true}>
+            <ScrollView style={{ flex: 1 }}>
               <View style={{marginBottom: 5,justifyContent: 'center', alignItems: 'center'}}>
                 <Text style={{color:'white'}}>Add Marks</Text>
               </View>
-              <View style={{marginBottom: 5}}>
+              {/* <View style={{marginBottom: 5}}>
                 <Text style={{color: 'white'}}>Subject Name</Text>
                   <TextInput
                   mode='flat'
@@ -91,13 +129,16 @@ export default function addPostScreen() {
                   selectionColor = 'white'
                   theme={{ colors: { text: 'white',placeholder: "white" } }}
                 />
-              </View>
+              </View> */}
+              {textInput.map((value, index) => {
+                  return value
+              })}
             <TouchableOpacity style={{alignItems: 'center',height: 40,marginTop: 10, backgroundColor: '#0088f8', justifyContent: 'center',
                 borderRadius: 5,}} onPress={toggleModal}>
                 <Text style={{color: '#fff'}}>Save</Text>
                 {/* <Icon style={Styles.loginText} name='chevron-right' type='font-awesome-5' color='#FFFFFF'/> */}
             </TouchableOpacity>
-          </View>
+          </ScrollView>
         </Modal>
       </View>
     </View>
