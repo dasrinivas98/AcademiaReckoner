809a073c6db019a0256966d35cd31344346ef7c2
dasrinivas98 <56031256+dasrinivas98@users.noreply.github.com>
2022-01-04 23:47:01 +0530

    Update addPostScreen.js
    Added dynamic Marks adding and Storing in JSON format

--- src/containers/main/addPost/addPostScreen.js
+++ src/containers/main/addPost/addPostScreen.js
@@ -13,8 +13,9 @@ export default function addPostScreen() {
   const [isModalVisible, setModalVisible] = useState(false);
   const [subCount,setSubCount] = useState(0);
   const [textInput,setTextInput] = useState([]);
-  
-
+  const [marks,setMarks] = useState(0);
+  const [sub,setSub] = useState("");
+  const [userInputs,setUserInputs] = useState([{"sub":"","marks":0,"credits":0}])
   const [items, setItems] = useState([
     {label: 1, value: 1},
     {label: 2, value: 2},
@@ -26,32 +27,76 @@ export default function addPostScreen() {
     {label: 8, value: 8},
 
   ]);
+  updateState = (index,k,value) => {
+    
+    const dt = [{"sem":index+1,"sub":value,"marks":k}]
+    console.log()
+    // console.log(k);
+    //const Textdata = userInputs; 
+    // let  dt = {index:{k:value}}
+    // console.log(dt);
+    //onChangeText={(text) => {setData({...data,usn:text})}}//make a copy of array
+    //setUserInputs([{...data,key:value}]);
+    //Textdata[index] = {key:value};
+    //setUserInputs({...userInputs,"sub":value,"marks":0});
+    // setUserInputs(Textdata);
+    console.log(dt);
+ }
+ const updateMarks = (index, value) => {
+  userInputs[index] = {...userInputs[index], ['marks']: value};
+  //setUserInputs({...userInputs[index],userInputs[index].marks: value});
+  /*setState(prevObj => ({
+    ...prevObj[index],
+    value1: value,
+  }));*/
+  // console.log(userInputs[0]);
+}
+
+const updateSubject = (index, value) => {
+  //state[index] = {...state[index], ['suub']: value};
+  userInputs[index] = {...userInputs[index], ['sub']: value};
+  //setUserInputs({...userInputs[index],['sub']: value});
+  /*setState(prevObj => ({
+    ...prevObj[index],
+    value2: value,
+  }));*/
+  // console.log(userInputs[0]);
+}
+const updateCredits = (index, value) => {
+  //state[index] = {...state[index], ['suub']: value};
+  userInputs[index] = {...userInputs[index], ['credits']: value};
+  //setUserInputs({...userInputs[index],['sub']: value});
+  /*setState(prevObj => ({
+    ...prevObj[index],
+    value2: value,
+  }));*/
+  // console.log(userInputs[0]);
+}
   const toggleModal = () => {
     let textInput1 = [];
     for(let i=0;i<subCount;i++){
-    textInput1.push(<View style={{marginBottom: 5}}key={i}>
+      textInput1.push(<View style={{marginBottom: 5}}key={i}>
       <Text style={{color: 'white'}}>Subject {i+1}</Text>
         <TextInput
-        key={i}
         mode='flat'
         style={{backgroundColor: 'black'}}
         activeOutlineColor='white'
         activeUnderlineColor = 'white'
         outlineColor = 'white'
         //underlineColor = 'white'
+        onChangeText={val => updateSubject(i, val)}
         selectionColor = 'white'
-        theme={{ colors: { text: 'white',placeholder: "white" } }}
+        theme={{colors: { text: 'white',placeholder: "white" }}}
       />
       <Text style={{color: 'white',marginTop:10}}>Credits Assigned</Text>
       <NumericInput
-        key={i}
         totalWidth={240} 
         totalHeight={50}
-          onChange={value => console.log(value)} rounded textColor='#59656F' iconStyle={{ color: '#59656F' }} rightButtonBackgroundColor='#292d3e' leftButtonBackgroundColor='#292d3e' minValue={1}
+          onChange={value => updateCredits(i,value)} 
+          rounded textColor='#59656F' iconStyle={{ color: '#59656F' }} rightButtonBackgroundColor='#292d3e' leftButtonBackgroundColor='#292d3e' minValue={1}
           maxValue={10}/>
       <Text style={{color: 'white',marginTop:10}}>Marks Obtained</Text>
         <TextInput
-        key={i}
         mode='flat'
         keyboardType='numeric'
         style={{backgroundColor: 'black'}}
@@ -59,6 +104,7 @@ export default function addPostScreen() {
         activeUnderlineColor = 'white'
         outlineColor = 'white'
         //underlineColor = 'white'
+        onChangeText={val => updateMarks(i,val)}
         selectionColor = 'white'
         theme={{ colors: { text: 'white',placeholder: "white" } }}
       />
@@ -67,6 +113,10 @@ export default function addPostScreen() {
     setTextInput(textInput1);
     setModalVisible(!isModalVisible);
   };
+  const saveMarks = () => {
+    console.log(userInputs);
+    setModalVisible(!isModalVisible);
+  }
 
   return (
     <View style={{flex: 1, backgroundColor: colors.bottomBackGround}}>
@@ -137,7 +187,7 @@ export default function addPostScreen() {
                   return value
               })}
             <TouchableOpacity style={{alignItems: 'center',height: 40,marginTop: 10, backgroundColor: '#0088f8', justifyContent: 'center',
-                borderRadius: 5,}} onPress={toggleModal}>
+                borderRadius: 5,}} onPress={saveMarks}>
                 <Text style={{color: '#fff'}}>Save</Text>
                 {/* <Icon style={Styles.loginText} name='chevron-right' type='font-awesome-5' color='#FFFFFF'/> */}
             </TouchableOpacity>
